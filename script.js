8a62e33a2d37484586c1fee994cb9d6db1585bc7
ALFA1422 <112336702+ALFA1422@users.noreply.github.com>
2023-06-26 21:01:24 +0200

    Retoques varios

--- script.js
+++ script.js
@@ -1,38 +1,77 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const headerLinks = document.querySelectorAll('header a');
-  const menuToggle = document.getElementById('menu-toggle');
+document.addEventListener('DOMContentLoaded', function () {
+  // Seleccionar todos los enlaces de la cabecera y las opciones del menú
+  const headerLinks = document.querySelectorAll('header a, header option');
+  // Obtener el elemento de cambio de menú
+  const menuToggle = document.getElementById('togglemenu');
 
-  headerLinks.forEach(function(link) {
-    link.addEventListener('click', function(event) {
+  // Función para marcar el enlace activo
+  function setActiveLink(targetId) {
+    headerLinks.forEach(function (link) {
+      // Eliminar la clase 'active' de todos los enlaces
+      link.classList.remove('active');
+      if (link.getAttribute('href') === targetId) {
+        // Agregar la clase 'active' al enlace correspondiente al targetId
+        link.classList.add('active');
+      }
+    });
+  }
+
+  // Manejar el evento de clic en los enlaces de la cabecera
+  headerLinks.forEach(function (link) {
+    link.addEventListener('click', function (event) {
       event.preventDefault();
       const targetId = this.getAttribute('href');
       const targetElement = document.querySelector(targetId);
 
       if (targetElement) {
         const offset = targetElement.offsetTop;
-        window.scrollTo({ top: offset, behavior: 'smooth' });
+        // Desplazamiento suave hasta el elemento objetivo
+        window.scrollTo({
+          top: offset,
+          behavior: 'smooth'
+        });
+        setActiveLink(targetId); // Marcar el enlace como activo
+        window.history.pushState(null, null, targetId); // Actualizar la URL con el nombre de la sección
       }
 
-      // Ocultar el menú desplegable después de hacer clic en un enlace
-      menuToggle.selectedIndex = 0;
+      if (menuToggle) {
+        // Establecer el índice seleccionado en función del targetId
+        menuToggle.selectedIndex = Array.from(menuToggle.options).findIndex(option => option.value === targetId);
+      }
     });
   });
 
-  menuToggle.addEventListener('change', function() {
-    const targetId = "#" + this.value;
-    const targetElement = document.querySelector(targetId);
+  // Manejar el evento de cambio en el menú desplegable
+  if (menuToggle) {
+    menuToggle.addEventListener('change', function () {
+      const targetId = "#" + this.value;
+      const targetElement = document.querySelector(targetId);
 
-    if (targetElement) {
-      const offset = targetElement.offsetTop;
-      window.scrollTo({ top: offset, behavior: 'smooth' });
-    }
+      if (targetElement) {
+        const offset = targetElement.offsetTop;
+        // Desplazamiento suave hasta el elemento objetivo
+        window.scrollTo({
+          top: offset,
+          behavior: 'smooth'
+        });
+
+        setActiveLink(targetId);
+        window.history.pushState(null, null, targetId);
+      }
+    });
+  }
+
+  // Manejar el evento de cambio en el historial del navegador (navegación hacia atrás/adelante)
+  window.addEventListener('popstate', function () {
+    const currentPath = window.location.hash;
+    setActiveLink(currentPath);
   });
 
-  // Agrega una clase "active" al enlace correspondiente en la botonera superior cuando se desplaza a una sección
-  window.addEventListener('scroll', function() {
+  // Manejar el evento de desplazamiento de la ventana
+  window.addEventListener('scroll', function () {
     const currentScrollPos = window.pageYOffset;
 
-    headerLinks.forEach(function(link) {
+    headerLinks.forEach(function (link) {
       const targetId = link.getAttribute('href');
       const targetElement = document.querySelector(targetId);
 
@@ -41,6 +80,7 @@ document.addEventListener('DOMContentLoaded', function() {
         const targetHeight = targetElement.offsetHeight;
 
         if (currentScrollPos >= targetOffsetTop && currentScrollPos < targetOffsetTop + targetHeight) {
+          // Agregar la clase 'active' al enlace correspondiente a la sección visible en la ventana
           link.classList.add('active');
         } else {
           link.classList.remove('active');
@@ -49,18 +89,3 @@ document.addEventListener('DOMContentLoaded', function() {
     });
   });
 });
-
-
-
-
-
-  
-  
-
-  
-  
-
-  
-  
-
-  
